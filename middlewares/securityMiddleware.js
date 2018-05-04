593494d1dad36d66f2abdb45cbf5e353bdf3700f
Ramiro Solis <h1.solis.castillo.ramiro@gmail.com>
2018-05-04 09:54:01 -0600

    Listo

--- middlewares/securityMiddleware.js
+++ middlewares/securityMiddleware.js
@@ -2,7 +2,7 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 
 function verifyToken(req, response, next){
-  const token = request.body.token || request.query.token || request.headers['x-access-token'];
+  const token = req.body.token || req.query.token || req.headers['x-access-token'];
   console.log(token);
   if(token){
     jwt.verify(token, '52d0380eb37d6d4666fddbd82daf5ee3', (err, decoded) => {
